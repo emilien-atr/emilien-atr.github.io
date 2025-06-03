eab7e8ce674024358cf818d406511eec78e4dc60
Emilien <emilien.autran@gmail.com>
2025-06-03 15:06:51 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -74,9 +74,4 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
-const menuToggle = document.querySelector('.menu-toggle');
-const navLinks = document.querySelector('.nav-links');
 
-menuToggle.addEventListener('click', () => {
-  navLinks.classList.toggle('show');
-});
