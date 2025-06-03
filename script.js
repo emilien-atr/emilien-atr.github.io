a27600578fa85cc74b1f7f168a0806d2899b9af8
Emilien <emilien.autran@gmail.com>
2025-06-03 15:01:54 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -72,3 +72,11 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 });
+
+
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
